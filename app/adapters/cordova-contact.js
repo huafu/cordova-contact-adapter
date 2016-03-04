9067d1486755e016ac830a86f77d6d52d93f1f2f
Peter Jackson <pete@peteonrails.com>
2016-03-04 17:35:04 -0500

    We're onyl concerned with displayname and phone numbers

--- app/adapters/cordova-contact.js
+++ app/adapters/cordova-contact.js
@@ -106,7 +106,7 @@ export default DS.Adapter.extend({
    */
   findAll: function (store, type, sinceToken) {
     return contactsLib().then(function (cdvContacts) {
-      var options = {filter: '', multiple: true, hasPhoneNumber: true};
+      var options = {filter: '', multiple: true, hasPhoneNumber: true, desiredFields: [navigator.contacts.fieldType.name, navigator.contacts.fieldType.phoneNumbers, navigator.contacts.fieldType.displayName]};
       return new Ember.RSVP.Promise(function (resolve, reject) {
         cdvContacts.find(
           [cdvContacts.fieldType.id],
